6b4b7f33a732ce4949087d79df19ecb7a5e27303
Diana <dianafrancitorraferran@gmail.com>
2024-03-05 13:32:05 +0100

    first commit

---
+++ Tasca_01/src/n1excersisi1/instruments/Instrument.java
@@ -0,0 +1,32 @@
+package n1excersisi1.instruments;
+
+public abstract class Instrument {
+    private String nom;
+    private float preu;
+
+    //constructor  que sera heredat
+    public Instrument(String nom, float preu) {
+        this.nom = nom;
+        this.preu = preu;
+    }
+    //metodes accessors
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public float getPreu() {
+        return preu;
+    }
+    public void setPreu(float preu) {
+        this.preu = preu;
+    }
+
+    static{
+        System.out.println("Carrega de la classe instrument");
+    }
+
+    //metode abstracte que simplementara en les classes filles
+    public abstract void tocar();
+}
