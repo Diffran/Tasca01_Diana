151ff635469efed808f5b94b223e3162411e9435
Diffran <148889182+Diffran@users.noreply.github.com>
2024-03-06 19:25:33 +0100

    Update VendaBuidaException.java
    tot nou des de casa

--- Tasca_01/Tasca_02/src/n1excersici1/botiga/VendaBuidaException.java
+++ Tasca_01/Tasca_02/src/n1excersici1/botiga/VendaBuidaException.java
@@ -1,13 +1,11 @@
 package n1excersici1.botiga;
 
 public class VendaBuidaException extends Exception{
-    private final String message ="Per fer una venda primer has d’afegir productes";
-    public VendaBuidaException(){
-        getMessage();
+    public VendaBuidaException(String message) {
+        super(message);
     }
-
     @Override
-    public String getMessage() {
-        return this.message;
+    public String getMessage(){
+        return super.getMessage();
     }
 }
