8843c32a0c74238d862f3b469afc79c34c9baf2b
Diffran <148889182+Diffran@users.noreply.github.com>
2024-03-06 19:24:54 +0100

    Update Venda.java
    Tot canviat a casa

--- Tasca_01/Tasca_02/src/n1excersici1/botiga/Venda.java
+++ Tasca_01/Tasca_02/src/n1excersici1/botiga/Venda.java
@@ -2,34 +2,29 @@ package n1excersici1.botiga;
 
 public class Venda {
     private Producte[] productes;
-    private double preuTotal;
-
-    public Venda(Producte[] productes) {
+    private float preuTotal;
+    
+    public Venda(Producte[] productes){
         this.productes = productes;
         calcularTotal();
     }
-
+//metodes accessors
     public Producte[] getProductes() {
         return productes;
     }
-
-    public void setProductes(Producte[] productes) {
-        this.productes = productes;
-    }
-
-    public double getPreuTotal() {
+    public float getPreuTotal() {
         return preuTotal;
     }
-
-    public void setPreuTotal(double preuTotal) {
+    public void setPreuTotal(float preuTotal) {
         this.preuTotal = preuTotal;
     }
-
+    //aquest metode sera cridat al constructor i inicilitzara latribut preuTotal
     public void calcularTotal(){
-        double suma = 0;
-        for(int i=0; i<getProductes().length;i++){
+        float suma=0;
+        for(int i=0; i<getProductes().length; i++){
             suma += getProductes()[i].getPreu();
         }
+        //em dona el total i li dono el valor al atribut de la classe
         setPreuTotal(suma);
     }
 }
