5d50e749fe65c9e4a30a1f9c344f858cace5ed9c
Diana <dianafrancitorraferran@gmail.com>
2024-03-07 13:55:02 +0100

    crisis ejercicio 3

---
+++ Tasca_01/Tasca_03/src/n1excersisi3/Main.java
@@ -0,0 +1,27 @@
+package n1excersisi3;
+
+import java.util.Scanner;
+import java.util.HashMap;
+public class Main {
+    public static void main(String[] args) {
+        String usuari;
+        Scanner lector = new Scanner(System.in);
+        String proves="";
+
+        System.out.println("Introdueixi el seu nom d'usuari: ");
+        usuari = lector.nextLine();
+        HashMap<String, String> countries = new HashMap<>();
+        countries.put("Espanya", "Madrid");
+        countries.put("França", "Paris");
+        countries.put("Portugal", "Lisboa");
+
+
+        //sha dutilitzar containsValue(Objecte value)
+
+
+        //sha de repetir 10 vegades
+        for(int i=0; i<10; i++){
+
+        }
+    }
+}
