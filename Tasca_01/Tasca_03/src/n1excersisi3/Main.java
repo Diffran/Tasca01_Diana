765eec2ca32b1508b2458689a94da6b96a81744f
Diffran <148889182+Diffran@users.noreply.github.com>
2024-03-11 08:47:30 +0100

    Update Main.java

--- Tasca_01/Tasca_03/src/n1excersisi3/Main.java
+++ Tasca_01/Tasca_03/src/n1excersisi3/Main.java
@@ -1,27 +1,103 @@
 package n1excersisi3;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Set;
+import java.util.HashSet;
+
 public class Main {
+    public static final String LINEA = "________________________________________________________________";
+
     public static void main(String[] args) {
-        String usuari;
-        Scanner lector = new Scanner(System.in);
-        String proves="";
+       Scanner lectorUsu = new Scanner(System.in);
+       HashMap<String, String> llistatCountries = new HashMap<>();
+       Random aleatori = new Random();
+       Set<Integer> repetits = new HashSet<>();
+           
+       String linea, valor, nomUsuari, rutaRelativa, resposta;
+       String[] partsLinea = new String[2];
+       int punts = 0, indexAleatori, contador = 0, index = 0;;
+       
+       //file del arxiu per llegir
+       rutaRelativa = "Tasca01_Diana/Tasca_01/dades/countries.txt";
+       File countries = new File (rutaRelativa);
 
-        System.out.println("Introdueixi el seu nom d'usuari: ");
-        usuari = lector.nextLine();
-        HashMap<String, String> countries = new HashMap<>();
-        countries.put("Espanya", "Madrid");
-        countries.put("França", "Paris");
-        countries.put("Portugal", "Lisboa");
-
-
-        //sha dutilitzar containsValue(Objecte value)
-
-
-        //sha de repetir 10 vegades
-        for(int i=0; i<10; i++){
-
-        }
+       //file del arxiu per escriure
+       rutaRelativa = "Tasca01_Diana/Tasca_01/dades/classificacio.txt";
+       File puntuacio = new File(rutaRelativa);
+       
+      
+       //Passar el file al HashMap----------------------------------------------
+       try{   
+          BufferedReader lector = new BufferedReader(new FileReader(countries.getAbsolutePath()));
+          //llegir el document
+          while((linea = lector.readLine()) != null){
+              partsLinea = linea.split(" ");
+              llistatCountries.put(partsLinea[0], partsLinea[1]);
+          }
+          lector.close();
+       }catch(IOException e){
+           System.out.println("Error: "+e.getMessage());
+       }
+       
+      //demanar al usuari les seves dades
+      System.out.println("introdueixi el seu nom d'usuari: ");
+      nomUsuari = lectorUsu.nextLine();
+      
+      //comença a mostrar de forma aleatoria 
+       System.out.println(LINEA);
+       System.out.println("\t\tENTRA LES CAPITALS");
+       System.out.println(LINEA);
+       
+       //Comença el bucle per treure de manera aleatoria els paisos
+       while(contador < 51){
+           //escull un número aleatori que no s'hagi repetit
+            indexAleatori = aleatori.nextInt(llistatCountries.size());
+            
+            while(repetits.contains(indexAleatori)){
+                indexAleatori = aleatori.nextInt(llistatCountries.size());
+            }
+           repetits.add(indexAleatori);
+            //si no el conté imprimeix a pantalla el pais a l'index escullit i espera l'entrada de dades del Usu
+            for (HashMap.Entry<String, String> entrada : llistatCountries.entrySet()) {
+                if (index == indexAleatori) {
+                    System.out.println(entrada.getKey()+": ");
+                    
+                    //recollir la resposta del Usuari
+                    resposta = lectorUsu.nextLine();
+                    
+                    //comporvar si es correcta, sumar punts si ho és
+                    if(entrada.getValue().equalsIgnoreCase(resposta)){
+                        System.out.println("Correcte!");
+                        punts++;
+                    }else{
+                        System.out.println("Incorrecte!");
+                    } 
+                }
+                index++;
+            }
+            index=0;//reiniciar per les seguents preguntes
+            contador++;
+       }       
+       
+       //diu a pantalla l'usuari i la puntuació
+       System.out.println(LINEA);
+       System.out.println("\t\tPUNTUACIÓ");
+       System.out.println(LINEA);
+       System.out.println(nomUsuari + "---> "+ punts);
+       
+       //Escriure el nom Usuari i la puntuació----------------------------------
+       try(FileWriter escriptor = new FileWriter(puntuacio.getAbsolutePath(), true)){
+           nomUsuari = nomUsuari + " " + punts + "\n";
+           escriptor.write(nomUsuari);
+       }catch(IOException e){
+           System.out.println("Error: "+e.getMessage());
+       }    
     }
 }
