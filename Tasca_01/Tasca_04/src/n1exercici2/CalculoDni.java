faba2f403356bac8158ee335a4e322aa5bd468ba
Diffran <148889182+Diffran@users.noreply.github.com>
2024-03-12 09:45:38 +0100

    Update CalculoDni.java sha de repasar i falta el test

--- Tasca_01/Tasca_04/src/n1exercici2/CalculoDni.java
+++ Tasca_01/Tasca_04/src/n1exercici2/CalculoDni.java
@@ -1,4 +1,11 @@
 package n1exercici2;
 
 public class CalculoDni {
+      static List<Character> letra = List.of('T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'));
+ 
+    public static char calculDni(int dni){
+        int index;
+        index = dni%23;
+        return letra.get(index);
+    }
 }
