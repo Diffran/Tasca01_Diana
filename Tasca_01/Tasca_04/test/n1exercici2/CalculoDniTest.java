57cde67f670cc58350e9575318f7607028535ca0
Diana <dianafrancitorraferran@gmail.com>
2024-03-12 12:41:57 +0100

    mes proves hi ha cata

--- Tasca_01/Tasca_04/test/n1exercici2/CalculoDniTest.java
+++ Tasca_01/Tasca_04/test/n1exercici2/CalculoDniTest.java
@@ -7,14 +7,16 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class CalculoDniTest {
-    public static final  int[] dni = new int[]{47964670,30151497,75135795,
-            76243131,81250943,12680260,51854026,55607297,24420826,55915411};
+    public static final  List dni = List.of(47964670,30151497,75135795,
+            76243131,81250943,12680260,51854026,55607297,24420826,55915411);
     public static final List<Character> lletra = List.of('X','S','Q','V','Q','S','C','J','R','L');
-
+    public static Stream<Integer> donarDni(){
+        return dni.stream();
+    }
     @ParameterizedTest
-    @ValueSource(dni)
+    @MethodSource("donarDni")
     public static void calculDniTest(Integer dni){
-
+        assertEqual("Hola","Hola");
     }
 
 }
