e84d7c919e3a245b8de77cf7e062b6898af7e8de
Diffran <148889182+Diffran@users.noreply.github.com>
2024-03-12 09:44:46 +0100

    Update Mesos.java a casa, no funciona el codi dha de retocar

--- Tasca_01/Tasca_04/src/n1exercici1/Mesos.java
+++ Tasca_01/Tasca_04/src/n1exercici1/Mesos.java
@@ -1,4 +1,16 @@
 package n1exercici1;
 
 public class Mesos {
+   List<String> mesos = new ArrayList<>();
+    mesos.addAll(List.of("gener","febrer","març","abril","maig","juny","juliol","agost","setembre","octubre","novembre","desembre"));
+
+}
+
+@Test 
+public void mesosTest(){
+    Mesos mes = new Mesos();
+    assertEquals(12,mes.size());
+    assertNotNull(mes);
+    assertEquals("agost",mes.getClass(7));
+    
 }
