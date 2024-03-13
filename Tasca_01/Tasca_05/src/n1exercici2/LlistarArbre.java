f1fc057a1cdc28a8bff3061a81e0606308517678
Diffran <148889182+Diffran@users.noreply.github.com>
2024-03-13 17:14:49 +0100

    Update LlistarArbre.java, fer que es vegi amb salts de linea si estan dins duna carpeta

--- Tasca_01/Tasca_05/src/n1exercici2/LlistarArbre.java
+++ Tasca_01/Tasca_05/src/n1exercici2/LlistarArbre.java
@@ -5,31 +5,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LlistarArbre {
-    //public static final String TAB = "\t";
+    public static final String TAB = "\t";
 
     public static void llistarArbre(String ruta){//posar parametre Integer de nivell
         File directori = new File(ruta);
-        /**
+        
         if(nivell==null || nivell<0){
             nivell=0;
         }
-        **/
+        
         if(directori.isDirectory()){
             File[] ordenarFiles = directori.listFiles();
             Arrays.sort(ordenarFiles);
 
             for(File file : ordenarFiles){
-                /**
                 //perque faci un o més tabs depenent del nivell
                 for(int i=0; i<nivell; i++){
                     System.out.print(TAB);
                 }
-                **/
+                
                 //imprimir el nom arxiu o entrar al directori
-                System.out.println(file.getName());
+                System.out.println("|"+file.getName());
                 if(file.isDirectory()){
-                    //nivell++;
-                    llistarArbre(file.getAbsolutePath());//faltara com a parametre el nivell
+                    nivell++;
+                    llistarArbre(file.getAbsolutePath(),nivell);
+                    nivell--;
                 }
             }
         }else{
