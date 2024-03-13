4e2d5db1e4dff96141c0fd9b7430fc15cd5e5644
Diana <dianafrancitorraferran@gmail.com>
2024-03-13 13:55:12 +0100

    ex3 tasca 05 encara falta coses

--- Tasca_01/Tasca_05/src/n1exercici3/LlistarArbreEscriure.java
+++ Tasca_01/Tasca_05/src/n1exercici3/LlistarArbreEscriure.java
@@ -1,22 +1,32 @@
 package n1exercici3;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class LlistarArbreEscriure {
     public static void llistarEsciureDir(String rutallegir, String rutaEscriure){
         File directori = new File(rutallegir);
+        File escriureDirectori = new File(rutaEscriure);
 
         if(directori.isDirectory()){
             File[] ordenarFiles = directori.listFiles();
             Arrays.sort(ordenarFiles);
 
-            for(File file : ordenarFiles){
-                //imprimir el nom arxiu o entrar al directori
-                System.out.println(file.getName());
-                if(file.isDirectory()){
-                    llistarEsciureDir(file.getAbsolutePath(), rutaEscriure);
+            try {
+                FileWriter escriptor = new FileWriter(escriureDirectori, true);
+                for (File file : ordenarFiles) {
+                    //imprimir el nom arxiu o entrar al directori
+                    escriptor.write(file.getName()+"\n");
+                    System.out.println(file.getName());
+                    if (file.isDirectory()) {
+                        llistarEsciureDir(file.getAbsolutePath(), rutaEscriure);
+                    }
                 }
+            }catch (IOException e){
+                System.out.println("Error: " + e.getMessage());
             }
         }else{
             System.out.println("el File no és un directori");
--- Tasca_01/Tasca_05/src/n1exercici3/Main.java
+++ Tasca_01/Tasca_05/src/n1exercici3/Main.java
@@ -2,6 +2,9 @@ package n1exercici3;
 
 public class Main {
     public static void main(String[] args) {
+        String directori;
+        String rutaEscriure;
 
+        LlistarArbreEscriure.llistarEsciureDir();
     }
 }
