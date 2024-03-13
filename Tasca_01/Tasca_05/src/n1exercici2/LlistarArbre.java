e2aaebe1f9ebb43c1ac69913281ef494b0a1f468
Diffran <148889182+Diffran@users.noreply.github.com>
2024-03-13 17:22:56 +0100

    Update LlistarArbre.java

--- Tasca_01/Tasca_05/src/n1exercici2/LlistarArbre.java
+++ Tasca_01/Tasca_05/src/n1exercici2/LlistarArbre.java
@@ -19,18 +19,21 @@ public class LlistarArbre {
             Arrays.sort(ordenarFiles);
 
             for(File file : ordenarFiles){
-                //perque faci un o més tabs depenent del nivell
+                
+                //perque faci un o més tabs depenent del nivell-------------------------------------
                 for(int i=0; i<nivell; i++){
                     System.out.print(TAB);
                 }
                 
-                //imprimir el nom arxiu o entrar al directori
+                //imprimir el nom arxiu o entrar al directori-----------------------------------------
                 System.out.println("|"+file.getName());
+                //si es un directori entra a dins i crida de nou el metode per imprimirne el contingut
                 if(file.isDirectory()){
                     nivell++;//fa que s'imprimeixi un tab mes
                     llistarArbre(file.getAbsolutePath(),nivell);
                     nivell--;//fa que s'imprimeixi un tab menys
                 }
+                
             }
         }else{
             System.out.println("el File no és un directori");
