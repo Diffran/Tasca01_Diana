96d242627b887ba203b8a90cad4d045c9491c6b2
Diana <dianafrancitorraferran@gmail.com>
2024-03-19 10:58:46 +0100

    exercici 7

--- Tasques/Tasca_08/src/n1exercici6/Main.java
+++ Tasques/Tasca_08/src/n1exercici6/Main.java
@@ -1,12 +1,23 @@
 package n1exercici6;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Object> llista = new ArrayList<>(List.of("Gato",2,"Elefante",67,
                 "Murciélago",1989,"Koala",73,"Tortuga",42));
+
+        ordenarStrings(llista);
+    }
+
+    public static void ordenarStrings(List<Object> llista){
+         llista.stream()
+                .filter(objecte -> objecte.getClass().equals(String.class))
+                .sorted(Comparator.comparingInt(paraula -> ((String)paraula).length()))
+                 .forEach(System.out::println);
 
     }
 }
--- Tasques/Tasca_08/src/n1exercici7/Main.java
+++ Tasques/Tasca_08/src/n1exercici7/Main.java
@@ -1,7 +1,22 @@
 package n1exercici7;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Object> llista = new ArrayList<>(List.of("Gato",2,"Elefante",67,
+                "Murciélago",1989,"Koala",73,"Tortuga",42));
 
+        ordenarStrings(llista);
+    }
+
+    public static void ordenarStrings(List<Object> llista){
+        llista.stream()
+                .filter(objecte -> objecte.getClass().equals(String.class))
+                //només cal demanar el metode .reversed un cop has utilitzat el comparator de length
+                .sorted(Comparator.comparingInt(paraula -> ((String)paraula).length()).reversed())
+                .forEach(System.out::println);
     }
 }
