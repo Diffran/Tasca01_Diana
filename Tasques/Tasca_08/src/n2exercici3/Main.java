a8d3f0137443a373eeac2d8dae59c6b54b53011f
Diana <dianafrancitorraferran@gmail.com>
2024-03-20 20:58:16 +0100

    n2 tasca 8 ex 3

--- Tasques/Tasca_08/src/n2exercici3/Main.java
+++ Tasques/Tasca_08/src/n2exercici3/Main.java
@@ -1,5 +1,32 @@
 package n2exercici3;
 
-public class Main {
+import org.w3c.dom.ls.LSOutput;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        float num1, num2;
+
+        System.out.println("Entra 2 numeros: ");
+        num1 = lector.nextFloat();
+        num2 = lector.nextFloat();
+
+        Operacio suma = () -> num1+num2;
+        Operacio resta = () -> num1-num2;
+        Operacio multiplicacio = () -> num1*num2;
+        Operacio divisio = () -> {
+            try{
+                return num2 != 0 ? num1/num2 : Float.NaN;
+            }catch(ArithmeticException e){
+                return Float.NaN;
+            }
+        };
+
+        System.out.println("Suma: " + suma.operacio());
+        System.out.println("Resta: " + resta.operacio());
+        System.out.println("Multiplicació: " + multiplicacio.operacio());
+        System.out.println("Divisió: " + divisio.operacio());
+    }
 }
