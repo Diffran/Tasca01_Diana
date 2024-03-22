ecb7573843406d2b9305ad0d433b8eafb3daa563
Diana <dianafrancitorraferran@gmail.com>
2024-03-22 20:15:57 +0100

    parametized amb @ValueSource

--- Tasques/Tasca_01/src/n1excercici1/Main.java
+++ Tasques/Tasca_01/src/n1excercici1/Main.java
@@ -9,15 +9,14 @@ public class Main {
     public static void main(String[] args) {
 
             System.out.println("Exercisi Instruments:");
-            //carrega la classe instanciant objectes
+
             Vent flauta = new Vent("flauta",3.2f);
             Vent trompeta = new Vent("trompeta", 2.2f);
-            trompeta.tocar(); //el metode abstracte de tocar instrument de vent
+            trompeta.tocar();
 
-            Corda.metodeStatic();//es carrega la classe corda perque es un metod static, sense instrument tocant
+            Corda.metodeStatic();
             Corda guitarra = new Corda("guitarra", 3.6f);
-            guitarra.tocar();//metode instrument de corda
-
+            guitarra.tocar();
             Percussio tambor = new Percussio("tambor", 4.6f);
             Percussio pandereta = new Percussio("pandereta", 1.0f);
             tambor.tocar();
--- Tasques/Tasca_04/test/n1exercici2/CalculoDniTest.java
+++ Tasques/Tasca_04/test/n1exercici2/CalculoDniTest.java
@@ -3,6 +3,8 @@ package n1exercici2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Stream;
@@ -26,6 +28,14 @@ public class CalculoDniTest {
     @ParameterizedTest
     @MethodSource("donarNumero")
     public void calculDniTest(Integer numero){
+        Assertions.assertEquals(inicializarDni().get(numero), CalculoDni.calculDni(numero));
+    }
+
+    //Arreglat
+    @ParameterizedTest
+    @ValueSource(ints = {47964670,30151497,75135795,
+            76243131,81250943,12680260,51854026,55607297,24420826,55915411})
+    public void calculDniTestValor(Integer numero){
         Assertions.assertEquals(inicializarDni().get(numero), CalculoDni.calculDni(numero));
     }
 }
